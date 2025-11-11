b6c31a366b8f4bc03dcec9989e42891a4c724ced
Rezuan Kassim <rezuankassim@hotmail.com>
2025-11-11 15:11:51 +0800

    remove unwanted routes

--- routes/web.php
+++ routes/web.php
@@ -10,10 +10,10 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
 
-require __DIR__.'/settings.php';
+// require __DIR__.'/settings.php';
